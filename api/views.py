a2344d968e6bdf633637808930d672cbc1fae632
harshraj2717 <harshraj2717@gmail.com>
2023-07-11 22:50:57 +0530

    Add base dev files for API

---
+++ api/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def login(request):
+    ...
+
+
+def register(request):
+    ...
+
+
+def password_change(request):
+    ...
