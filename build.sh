941b49ea21d7ac501766868f5719154d8d963101
harshraj2717 <harshraj2717@gmail.com>
2023-07-15 03:30:43 +0530

    update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 pip install -r requirements.txt
 poetry install
-python manage.py collectstatic --no-input
+python manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py createsuperuser --noinput
