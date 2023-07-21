529e018d4c10e9a8e9be20a7a7d49c8208030170
harshraj2717 <harshraj2717@gmail.com>
2023-07-22 00:50:54 +0530

    add cloud postgreSQL database

--- api_server/settings.py
+++ api_server/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import dj_database_url
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -19,6 +20,7 @@ load_dotenv()
 EMAIL_ADDRESS = os.getenv('qtools_sender_email')
 EMAIL_PASSWORD = os.getenv('qtools_sender_email_password')
 FRONTEND_SECRET_KEY = os.getenv('frontend_secret_key')
+DATABASE_URL = os.getenv('database_url')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -100,10 +102,10 @@ WSGI_APPLICATION = 'api_server.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+    )
 }
 
 
