bce43507ab9f4aaf70bc714046a76582b889feee
harshraj2717 <harshraj2717@gmail.com>
2023-08-11 00:10:14 +0530

    switch debug database to local database

--- api_server/settings.py
+++ api_server/settings.py
@@ -103,16 +103,24 @@ WSGI_APPLICATION = 'api_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'neondb',
-        'USER': NEON_DB_USERNAME,
-        'PASSWORD': NEON_DB_PASSWORD,
-        'HOST': NEON_DB_HOST,
-        'PORT': NEON_DB_PORT,
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'neondb',
+            'USER': NEON_DB_USERNAME,
+            'PASSWORD': NEON_DB_PASSWORD,
+            'HOST': NEON_DB_HOST,
+            'PORT': NEON_DB_PORT,
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
